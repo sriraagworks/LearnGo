bcc457ea4897324cd4a98d50fba609bbd4b4e08f
sriraagworks <sriraagworks@gmail.com>
2020-07-19 11:33:20 +0530

    adding variables in go example program

--- learn_variables.go
+++ learn_variables.go
@@ -5,14 +5,19 @@ import "fmt"
 func learnvariablesMain() {
 
 	/*Variable declaration going on here*/
-
+	var z string /*Pre declaring variables*/
 	var id int = 573
-	name := "Sriraag"
-	var x, y = "1234", false
+	name := "Sriraag"           /*shorthand variable declaration*/
+	var x, y = "1234", false    /*declaring  multiple variables*/
+	z = "Pre Declared Variable" /*assigning  values  for predeclared variables */
+	/*Need to  read  up  on blank identifiers in go
+	Go will not allow us to run a  program which has declared variables which  remain unused
+	*/
 
 	fmt.Println("Student-ID::", id)
 	fmt.Println("Student-Name::", name)
-	fmt.Println("X Value::", x)
-	fmt.Println("Y Value::", y)
+	fmt.Println("x Value::", x)
+	fmt.Println("y Value::", y)
+	fmt.Println("z Value::", z)
 
 }
