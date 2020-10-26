9769948a5db73d7857500377228edc84d0f6431c
Sriraag Sridhar <53432553+sriraagworks@users.noreply.github.com>
2020-10-26 14:36:31 +0530

    Adding delete function line to delete a value
    Adding delete function line to delete a value from an initialized map based on the key

--- learn_maps.go
+++ learn_maps.go
@@ -5,16 +5,18 @@ import "fmt"
 func main(){
 
 //// Maps in Go
-
+  //variable_name:=map[key_var_type]value_type
 emp_details:=map[int]string{
 1:"luffy",
 2:"goku",
 3:"uarmeshi",
+4:"xyz",
 }
 
 fmt.Println(emp_details)
 
-}
+delete(emp_details,4) //Deletes a Value from emp_details map
 
-//Output
-///map[1:luffy 2:goku 3:uarmeshi]
+fmt.Println(emp_details)
+
+}
