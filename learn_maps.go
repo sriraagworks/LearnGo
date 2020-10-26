acfb340acf8643f5e548690d6f8626a7076854b5
Sriraag Sridhar <53432553+sriraagworks@users.noreply.github.com>
2020-10-26 14:30:30 +0530

    Maps in Go

---
+++ learn_maps.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//// Maps in Go
+
+emp_details:=map[int]string{
+1:"luffy",
+2:"goku",
+3:"uarmeshi",
+}
+
+fmt.Println(emp_details)
+
+}
+
+//Output
+///map[1:luffy 2:goku 3:uarmeshi]
