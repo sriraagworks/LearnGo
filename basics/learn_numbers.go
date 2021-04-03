fa54a1a0be837640d0eac3303bf5863d547925c2
sriraagworks <sriraagworks@gmail.com>
2021-04-03 22:11:20 +0530

    Created Folders for better uniformity and will be good for data-structure related operations

---
+++ basics/learn_numbers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func learnmathMain() {
+	fmt.Println("Addition 5+5  =", 5+5)
+	fmt.Println("Subtraction 5-5 =", 5-5)
+	fmt.Println("Multiplication 5*5 =", 5*5)
+	fmt.Println("Division 100/5 =", 100/5)
+	fmt.Println("Exponent", math.Pow(1.5, 5))
+}
